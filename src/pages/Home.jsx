161df3eb9775b44550add393ca8695c7ea0d8f7f
DilutedPilk <myawesome4@gmail.com>
2026-07-28 15:36:26 -0400

    Dark mode functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import SearchBar from "../components/SearchBar.jsx";
 import ItemList from "../components/ItemList.jsx";
+import { ThemeContext } from "../context.js";
+
+import { useContext, createContext } from "react";
 
 export default function Home({ items, favoriteItems, toggleFavorite }) {
+
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   function handleSearch(searchText) {
     console.log(searchText);
   }
 
   return (
-    <div>
+    <div className={theme == 'light'? 'bg-white':'bg-mist-600 text-white'}>
       <h1 className="flex justify-center items-center m-5">Cat Search Website</h1>
       <SearchBar onSearch={handleSearch} />
       <ItemList items={items}
