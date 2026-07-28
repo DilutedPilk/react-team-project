9491c639d2ab9f2c75bc88e94727c996a60c71d0
Jacky <jackylai1030@gmail.com>
2026-07-28 15:15:31 -0400

    added cs to favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,8 +2,8 @@ import ItemList from "../components/ItemList.jsx";
 
 export default function Favorites({ favoriteItems, toggleFavorite }) {
   return (
-    <div className="favorites">
-      <h2>Your Favorites</h2>
+    <div className="p-6 min-h-screen">
+      <h2 className="text-center text-3xl font-bold text-rose-400 mb-8">Your Favorites</h2>
       <ItemList
         items={favoriteItems}
         favoriteItems={favoriteItems}
