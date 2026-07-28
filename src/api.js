5058ab3895b6a604173cb57348f4d2348dfacffd
DilutedPilk <myawesome4@gmail.com>
2026-07-28 11:45:46 -0400

    made api.js functions

--- src/api.js
+++ src/api.js
@@ -1,4 +1,21 @@
-export default async function getCats(query){
-    
-    const request = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${query}&api_key=${import.meta.env.VITE_API_KEY}`)
+async function getCats(query){
+    try {
+        const request = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${query}&api_key=${import.meta.env.VITE_API_KEY}`)
+        const result = await request.json();
+        return result
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+async function getCatsByID(id){
+        try {
+        const request = await fetch(`https://api.thecatapi.com/v1/images/${id}`)
+        const result = await request.json();
+        return result
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default { getCatsByID, getCats }
